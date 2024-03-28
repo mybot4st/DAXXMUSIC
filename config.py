3b8a8be3314af22c9191f8a6cc364955ffc8fab2
mybot4st <164684936+mybot4st@users.noreply.github.com>
2024-03-29 02:48:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DAXXSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/codex_ka_baap_4st")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/i_m_fighter")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
